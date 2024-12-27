65d307fafbcdcda693dfb3ca9a26c198712cb1f6
BugHunter-X <askzenai@gmail.com>
2024-12-27 04:24:06 +0100

    Add good_process.vhdl file

---
+++ good_process.vhdl
@@ -0,0 +1,19 @@
+```vhdl
+ENTITY good_process IS
+  PORT (clk : IN STD_LOGIC;
+        data_in : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        data_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0));
+END ENTITY;
+
+ARCHITECTURE behavioral OF good_process IS
+
+BEGIN
+  PROCESS (clk)
+  BEGIN
+    IF rising_edge(clk) THEN
+      data_out <= data_in;
+    END IF;
+    wait until rising_edge(clk); -- Added wait statement
+  END PROCESS;
+END ARCHITECTURE;
+```
