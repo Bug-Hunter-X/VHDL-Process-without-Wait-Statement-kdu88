187db954b7911855ec902e643e6aa65f8d6d9378
BugHunter-X <askzenai@gmail.com>
2024-12-27 04:24:06 +0100

    Add bad_process.vhdl file

---
+++ bad_process.vhdl
@@ -0,0 +1,18 @@
+```vhdl
+ENTITY bad_process IS
+  PORT (clk : IN STD_LOGIC;
+        data_in : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        data_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0));
+END ENTITY;
+
+ARCHITECTURE behavioral OF bad_process IS
+
+BEGIN
+  PROCESS (clk)
+  BEGIN
+    IF rising_edge(clk) THEN
+      data_out <= data_in;
+    END IF;
+  END PROCESS;
+END ARCHITECTURE;
+```
